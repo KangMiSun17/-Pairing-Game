abcb875537703d810105bc44ecb11512a26a35e3
ms <altjsvnf@naver.com>
2022-02-09 22:18:44 +0900

    카드 객체화, 타이머 구현, 리셋 구현

--- main.js
+++ main.js
@@ -1,59 +1,105 @@
 //변수 선언
 let current = 0; //현재 뒤집은 카드
 let total = 0; //다 뒤집은 카드
-let score = 0; //점수
-let cards = ['/IMG/10.png','/IMG/10.png', '/IMG/J.png', '/IMG/J.png', '/IMG/Q.png', '/IMG/Q.png', '/IMG/K.png','/IMG/K.png','/IMG/A.png','/IMG/A.png']; //카드 목록
+let time = 0; //시간
+let cards = [
+    { 
+        img: '/IMG/10.png',
+        isOpen: false
+    },
+    {
+        img: '/IMG/10.png',
+        isOpen: false
+     },
+     {
+         img: '/IMG/J.png',
+         isOpen: false
+     },
+    {
+        img: '/IMG/J.png',
+        isOpen: false
+     },
+     {
+        img: '/IMG/Q.png',
+        isOpen: false
+     },
+     {
+         img: '/IMG/Q.png',
+         isOpen: false
+      },
+    {
+        img: '/IMG/K.png',
+        isOpen: false
+    },
+    {
+        img: '/IMG/K.png',
+        isOpen: false
+    },
+    {
+        img: '/IMG/A.png',
+        isOpen: false
+    },
+    {
+        img: '/IMG/A.png',
+        isOpen: false
+    }]; //카드 목록
 let pair = [] //짝 카드
-let how_score = document.querySelector('p');
 
-//카드 뒷면 생성
+//카드 생성
 let cardNum = new Array(cards.length);
 let space = document.querySelector('.card_space');
 for(let i=0; i<cardNum.length; i++){
     cardNum[i] = document.createElement('img');
-    cardNum[i].src = "/IMG/뒷면.png";
     cardNum[i].id = i;
     space.appendChild(cardNum[i]);
 }
 
+//카드 확인
+function FlipCard (e) {
+    if(cards[e.target.id].isOpen){
+        e.target.src = cards[e.target.id].img
+    }else{
+        e.target.src = '/IMG/뒷면.jpg'
+    }
+}
+
 //카드 섞기
-cards.sort(function() {
-    return Math.random() - Math.random()})
+function shuffle () {
+    cards.sort(() => Math.random() - Math.random())
+}
 
 //카드 뒤집기
 space.addEventListener('click', (e) => {
-    rotation(e);
-    let what = cards[e.target.id];
-    e.target.src = what;
-    pair[current] = what;
-    current++;
-    if(current === 2){
-        compare(e);
-    }else{
-        
+    cards[e.target.id].isOpen = true;
+    console.log(e.target)
+    pair[current] = cards[e.target.id].img
+    current++
+    if(current==2){
+        compare(e)
     }
+    rotation(e);
+    FlipCard(e);
     finish();
+    console.log(pair)
 })
 
 //카드 비교
 compare = (e) => {
     if(pair[0]===pair[1]){
         total += 2;
-        score += 1;
-        how_score.innerHTML = `점수 : ${score}`
+        current = 0;
+        pair.length = 0;
     }else{
-        setTimeout(()=>{
-            rotation(e)
-            e.target.src = '/IMG/뒷면.png'}
-            ,2000)
+        current = 0;
+        pair.length = 0;
     }
-    current = 0;
 }
 
 //게임 끝났는지 확인
 finish = () => {
-    if(total == cards.length){
-        alert(`당신의 점수는 ${score}점 입니다!`);
+    if(total === cards.length){
+        alert(`축하합니다! ${time}초 만에 클리어!`)
+        reset()
     }
 }
 
@@ -65,3 +111,32 @@ rotation = (e) => {
         e.target.style.transform = "rotateY(360deg)";
     }
 }
+
+//리셋
+
+reset = () => {
+    for(i=0; i<cards.length; i++){
+        cardNum[i].src = '/IMG/뒷면.jpg'
+    }
+    total = 0;
+    time = 0;
+}
+
+//타이머
+let whatTime = document.querySelector('p');
+
+function timer() {
+    time = setInterval(() => {
+        whatTime.innerText = `시간 : ${time}`
+        time++;
+    }, 1000)
+}
+
+//게임시작
+function start () {
+    reset()
+    shuffle();
+    timer()
+}
+
+start()
